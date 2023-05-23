0ab75fef5cc746aae1d37fb31d95fbdee0d7b985
Mahedi3110 <shuvo@shuvo>
2023-05-23 23:13:23 +0600

    almost done

--- src/components/body/MyProduct.jsx
+++ src/components/body/MyProduct.jsx
@@ -13,6 +13,7 @@ const MyProduct = () => {
 
     return (
         <div className='bg-black h-screen pt-32 px-24'>
+
             <ol
                 data-aos="fade-up"
                 data-aos-easing="linear"
